bb841560c957c7d29a7778c57a155fd7fd616a22
Kasper Mikiewicz <IderedPL@gmail.com>
2019-06-27 20:31:50 +0200

    chore: force styled components macro

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -141,6 +141,18 @@ module.exports = {
     'no-labels': ['warn', { allowLoop: true, allowSwitch: false }],
     'no-lone-blocks': 'warn',
     'no-loop-func': 'warn',
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'styled-components',
+            message: 'Please import from styled-components/macro.',
+          },
+        ],
+        patterns: ['!styled-components/macro'],
+      },
+    ],
     'no-mixed-operators': [
       'warn',
       {
