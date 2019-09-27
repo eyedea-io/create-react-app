1ad6c7f8f3782f3c30b51ffac3b2a37b298ef78a
Kasper Mikiewicz <IderedPL@gmail.com>
2019-09-27 12:30:11 +0200

    feat: add collection

--- packages/react-scripts/template-typescript/workspaces/app/store.ts
+++ packages/react-scripts/template-typescript/workspaces/app/store.ts
@@ -1,18 +1,21 @@
 import {types, Instance} from 'mobx-state-tree'
 import * as models from '@app/models'
+import * as customTypes from '@app/utils/custom-types'
 import {createContext} from 'react'
 
-export const RootStore = types.model('RootStore', {
-  profile: types.maybe(models.Profile),
-  token: types.maybe(types.string),
-  loremIpsum:
-    'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.'
-})
-.views(self => ({
-  get isLoggedIn(): boolean {
-    return Boolean(self.profile && self.token)
-  }
-}))
+export const RootStore = types
+  .model('RootStore', {
+    profile: types.maybe(models.Profile),
+    token: types.maybe(types.string),
+    users: customTypes.collection(models.Profile, 'Users'),
+    loremIpsum:
+      'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.'
+  })
+  .views(self => ({
+    get isLoggedIn(): boolean {
+      return Boolean(self.profile && self.token)
+    }
+  }))
 
 export const store = RootStore.create()
 export const RootStoreContext = createContext<Instance<typeof RootStore>>(store)
--- packages/react-scripts/template-typescript/workspaces/app/utils/custom-types.tsx
+++ packages/react-scripts/template-typescript/workspaces/app/utils/custom-types.tsx
@@ -1,5 +1,12 @@
-import {types, IAnyType, SnapshotOrInstance, Instance} from 'mobx-state-tree'
+import {
+  types,
+  IAnyType,
+  SnapshotOrInstance,
+  Instance,
+  destroy
+} from 'mobx-state-tree'
 import dayjs, {Dayjs} from 'dayjs'
+import {moveItem} from './move-item'
 
 export const date = types.custom<string | number, Dayjs>({
   name: 'CustomDate',
@@ -17,51 +24,208 @@ export const date = types.custom<string | number, Dayjs>({
   }
 })
 
-export function array<T extends IAnyType>(model: T, name: string) {
+export function collection<T extends IAnyType>(model: T, name: string) {
   const base = types
     .model(name, {
+      /**
+       * Collection elements.
+       */
       items: types.array(model)
     })
     .views(self => ({
       /**
-       * Check if items array is empty
+       * Get all collection items.
        */
-      empty() {
+      all() {
+        return self.items
+      },
+      /**
+       * Check if collection is empty.
+       */
+      isEmpty() {
         return self.items.length === 0
       },
       /**
-       * Check if current items contains element with given id
+       * Check if collection is not empty.
        */
-      has(id?: number | string) {
+      isNotEmpty() {
+        return self.items.length !== 0
+      },
+      /**
+       * Get items identifiers.
+       */
+      keys(): number[] {
+        return self.items.map(item => item.id)
+      },
+      /**
+       * Check if collection contains item with given id.
+       */
+      has(id?: number) {
         return self.items.some(item => item.id === id)
       },
       /**
-       * Get first element of array
+       * Get first element of collection.
        */
       first(): Instance<T> | null {
         return self.items.length ? self.items[0] : null
+      },
+      /**
+       * Get last element of collection.
+       */
+      last(): Instance<T> | null {
+        return self.items.length ? self.items[self.items.length - 1] : null
+      },
+      /**
+       * Get the total number of items in the collection.
+       */
+      count(): number {
+        return self.items.length
+      },
+      /**
+       * Get the number of items that match the filter check.
+       *
+       * @example
+       * // Count all items
+       * store.posts.countBy()
+       * // Count items by author id
+       * store.posts.countBy(item => item.author.id === 1)
+       */
+      countBy(filter: (item: SnapshotOrInstance<T>) => any): number {
+        if (typeof filter === 'function') {
+          return self.items.filter(filter).length
+        }
+
+        return self.items.length
+      },
+      /**
+       * Get a console.log of items.
+       * @example
+       * store.users.dump()
+       */
+      dump() {
+        console.log(self.items.toJSON())
+      },
+      /**
+       * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+       */
+      map<U>(
+        callbackfn: (
+          value: Instance<T>,
+          index: number,
+          array: Instance<T>[]
+        ) => U,
+        thisArg?: any
+      ) {
+        return self.items.map(callbackfn, thisArg)
+      },
+      /**
+       * Performs the specified action for each element in an array.
+       */
+      forEach(
+        callbackfn: (
+          value: Instance<T>,
+          index: number,
+          array: Instance<T>[]
+        ) => void,
+        thisArg?: any
+      ) {
+        return self.items.forEach(callbackfn, thisArg)
       }
     }))
     .actions(self => ({
       /**
-       * Remove all items
+       * Remove all items.
+       * @example
+       * store.users.clear()
        */
       clear() {
         self.items.replace([])
       },
       /**
-       * Replace current items with given items
+       * Replace collection items with given items.
+       * store.users.set([{id: 1, username: 'john'}])
        */
-      replace(items: SnapshotOrInstance<T>[]) {
+      set(items: SnapshotOrInstance<T>[]) {
         self.items.replace(items)
+      },
+      /**
+       * Find item by id.
+       * @example
+       * store.users.get(2)
+       */
+      get(id: number) {
+        return self.items.find(item => item.id === id)
+      },
+      /**
+       * Add single or multiple items to collection.
+       * @example
+       * store.posts.add({id: 1, title: 'Hello'})
+       * store.posts.add([
+       *   {id: 1, title: 'Hello'},
+       *   {id: 2, title: 'World'},
+       * ])
+       */
+      add(model: SnapshotOrInstance<T> | SnapshotOrInstance<T>[]) {
+        if (Array.isArray(model)) {
+          self.items.push(...model)
+        } else {
+          self.items.push(model)
+        }
+      },
+      /**
+       * Remove item by passing model, array of models or id.
+       * @example
+       * store.users.remove(user)
+       * store.users.remove([me, otherUser])
+       * store.users.remove(1)
+       * store.users.remove([1, 2])
+       */
+      remove(
+        model:
+          | SnapshotOrInstance<T>
+          | SnapshotOrInstance<T>[]
+          | number
+          | number[]
+      ) {
+        const removeByModelOrByNumber = (
+          idOrModel: number | SnapshotOrInstance<T>
+        ) => {
+          if (typeof idOrModel === 'number') {
+            const itemInCollection = self.items.find(
+              item => item.id === idOrModel
+            )
+            if (itemInCollection) destroy(itemInCollection)
+          } else {
+            destroy(idOrModel)
+          }
+        }
+
+        if (Array.isArray(model)) {
+          model.forEach(removeByModelOrByNumber)
+        } else {
+          removeByModelOrByNumber(model)
+        }
+      },
+      /**
+       * Moves an item from one position to another, checking that the indexes given are within bounds.
+       * @example
+       * store.posts.move(1, 2)
+       */
+      move(fromIndex: number, toIndex: number) {
+        moveItem(self.items, fromIndex, toIndex)
       }
     }))
     .actions(self => ({
       /**
        * Load data from given request and inject it into items
+       * @example
+       * store.posts.load(endpoints.posts.list, {author: 1})
        */
-      load: async <Args,>(request: (args: Args) => Promise<SnapshotOrInstance<T>[]>, args: Args) => {
-        return request(args).then(self.replace)
+      load: async function<Args>(
+        request: (args: Args) => Promise<SnapshotOrInstance<T>[]>,
+        args: Args
+      ) {
+        return request(args).then(self.set)
       }
     }))
   return types.optional(base, {items: []})
